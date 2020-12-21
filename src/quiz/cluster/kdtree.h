53e2b2eb8e275820f808dccb1e971d1cae2266d9
josper <hcv1027@gmail.com>
2020-12-21 16:56:57 +0800

    Complete kdtree search function

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -3,17 +3,19 @@
 
 #include <list>
 #include <memory>
+#include <utility>
 #include "../../render/render.h"
 
 // Structure to represent node of kd tree
 struct Node {
   std::vector<float> point;
   int id;
+  int split_axis;  // 0: x-axis, 1: y-axis
   Node* left;
   Node* right;
 
-  Node(std::vector<float> arr, int setId)
-      : point(arr), id(setId), left(NULL), right(NULL) {}
+  Node(std::vector<float> arr, int setId, int split)
+      : point(arr), id(setId), split_axis(split), left(NULL), right(NULL) {}
   ~Node() { std::cout << "Node " << id << " is released." << std::endl; }
 };
 
@@ -46,12 +48,42 @@ struct KdTree {
       depth++;
     }
     std::cout << "insert " << id << " to depth " << depth << std::endl;
-    *curr_node = new Node(point, id);
+    *curr_node = new Node(point, id, depth % 2);
   }
 
   // return a list of point ids in the tree that are within distance of target
   std::vector<int> search(std::vector<float> target, float distanceTol) {
     std::vector<int> ids;
+
+    if (root != NULL) {
+      std::list<std::pair<int, Node*>> searchList;
+      searchList.push_front(std::make_pair(0, root));
+      while (!searchList.empty()) {
+        int depth = searchList.front().first;
+        Node* node = searchList.front().second;
+        searchList.pop_front();
+        if (std::fabs(target[0] - node->point[0]) < 2 * distanceTol &&
+            std::fabs(target[1] - node->point[1]) < 2 * distanceTol) {
+          float dist = std::hypot(target[0] - node->point[0],
+                                  target[1] - node->point[1]);
+          if (dist < distanceTol) {
+            ids.push_back(node->id);
+          }
+        }
+
+        // Check accross boundary
+        int dim = depth % 2;
+        if (target[dim] + distanceTol > node->point[dim] &&
+            node->right != NULL) {
+          searchList.push_front(std::make_pair(depth + 1, node->right));
+        }
+        if (target[dim] - distanceTol < node->point[dim] &&
+            node->left != NULL) {
+          searchList.push_front(std::make_pair(depth + 1, node->left));
+        }
+      }
+    }
+
     return ids;
   }
 
